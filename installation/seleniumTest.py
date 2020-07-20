25b8002f0cac802c0fa169d2c51fcc7ff4a21e6c
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2020-07-19 21:47:36 -0700

    updated install stuff

--- installation/seleniumTest.py
+++ installation/seleniumTest.py
@@ -1,9 +1,10 @@
 # Test program to see if selenium works
-# You should see a google chrome window pop up 
+# You should see a google chrome window pop up and then close
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.close()
 
 print("if you can read this, selenium is working")
