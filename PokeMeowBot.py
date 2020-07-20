8b48fa4eb53daf306b852763c29ba7f18795ff29
DerekQuoc <derekquoc@gmail.com>
2020-07-19 21:25:52 -0700

    Updated needed commands and functions

---
+++ PokeMeowBot.py
@@ -0,0 +1,14 @@
+#1.Request a pokemon encounter
+    #a send ;p message every 7-20seconds (rand)
+#2. Find/verify response message
+#3.Check if there is bot check message and respond if not
+    #3.Respond with appropriate ball
+#4.Check amount of balls and restock if necessary
+    #a reading amount of balls
+    #b using buy command knowing how much each ball costs
+
+
+
+#1 Read message
+#2 Figure out response
+#3 Send response
