fe712cefd465b3290dae2d2d4483722e3eef7522
DerekQuoc <derekquoc@gmail.com>
2020-07-22 00:01:21 -0700

    Update PokeMeowLogic.py

--- PokeMeowLogic.py
+++ PokeMeowLogic.py
@@ -1,15 +1,3 @@
-# this file is where all the game-playing logic goes
-
-#1.Request a pokemon encounter
-    #a send ;p message every 7-20seconds (rand)
-#2. Find/verify response message
-#3.Check if there is bot check message and respond if not
-    #3.Respond with appropriate ball
-#4.Check amount of balls and restock if necessary
-    #a reading amount of balls
-    #b using buy command knowing how much each ball costs
-
-import time
 class PokeMeowBotLogic():
     Coins = 0
     Pokeballs = 0
